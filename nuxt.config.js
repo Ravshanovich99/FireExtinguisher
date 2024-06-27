bfa6337fad55e72b1524bfd8d35b899daa6696d7
Rustam <niwonboyevrustam@gmail.com>
2024-06-27 15:39:54 +0500

    adopting to custom domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,4 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-
   head: {
     title: 'Florida',
     htmlAttrs: {
@@ -27,13 +25,11 @@ export default {
 
   target: 'static',
   router: {
-    base: '/FloridaGifts/',
+    base: '/', // Correct base URL for the root of the custom domain
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['./assets/main.scss'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {
       src: './plugins/vue-icons.js',
@@ -41,21 +37,12 @@ export default {
     },
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/dotenv',
-  ],
+  buildModules: ['@nuxtjs/eslint-module', '@nuxtjs/dotenv'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     [
       '@nuxtjs/firebase',
@@ -85,13 +72,10 @@ export default {
     ],
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     babel: {
       compact: true,
